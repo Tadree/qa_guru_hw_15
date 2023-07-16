e50299784e34abb856a26c2abd57563e3f9c1eb9
Elena Kosiakova <Elen.Nikitina6@x5.ru>
2023-07-16 15:10:34 +0200

    update build.gradle

--- src/test/java/ru/jiehk/autotests/pages/SearchResultsPage.java
+++ src/test/java/ru/jiehk/autotests/pages/SearchResultsPage.java
@@ -6,7 +6,6 @@ import io.qameta.allure.Step;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static com.sun.tools.doclint.Entity.or;
 
 public class SearchResultsPage {
     private SelenideElement
